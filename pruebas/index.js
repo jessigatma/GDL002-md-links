015cd4b7888093fbc606300d8819f1ba466dd984
Your Name <you@example.com>
2019-04-15 11:19:29 -0500

    eliminando carpeta pruebas

--- pruebas/index.js
+++
@@ -1,8 +0,0 @@
-const math = require('./math.js');
-
-console.log(math);
-
-// console.log(math.add(1,0));
-// console.log(math.multiply(1,0));
-// console.log(math.substract(1,0));
-// console.log(math.divide(1,2));
